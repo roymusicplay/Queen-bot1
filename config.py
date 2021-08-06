88f150485028ba5ce511c8b42dde220c7562c67a
ABHINAS ROY <85425565+roymusicplay@users.noreply.github.com>
2021-08-06 16:40:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ from heroku3 import from_key
 from pyrogram import Client
 
 API_ID = int(os.environ.get("API_ID", "6"))
-API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+API_HASH = os.environ.get("API_HASH", "ff8ee60e4b9f66552ee0546e47c5565b")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-ARQ_API_KEY = "XQYJAL-HTSZIK-YALWDS-TJPWMO-ARQ" 
-LANGUAGE = "en"
+ARQ_API_KEY = "BMVCMF-YGSKKU-QEBOVE-NNETKU-ARQ" 
+LANGUAGE = "en" "hi"
 ARQ_API_BASE_URL = "https://thearq.tech"
 
 bot = Client(":memory:",
